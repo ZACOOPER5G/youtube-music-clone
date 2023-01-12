118938fac86ab4cb806f8a0908d75d8f7f742ca8
Zac Cooper <zacooper5g@outlook.com>
2023-01-12 14:47:50 +1100

    added imagery in and basic styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import React, { useContext, useEffect } from "react";
 import MusicContext from "../context/MusicContext";
+import Image from "next/image";
 
 const getToken = async () => {
 	let response = await fetch("https://accounts.spotify.com/api/token", {
@@ -18,7 +19,7 @@ const getToken = async () => {
 	return data.access_token;
 };
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
 	let token = await getToken();
 	let response = await fetch(`https://api.spotify.com/v1/browse/categories`, {
 		headers: {
@@ -39,8 +40,8 @@ export default function Home({ data }: any) {
 	const { categories, setCategories } = musicContext;
 
 	useEffect(() => {
-		setCategories(data);
-	}, []);
+		if (typeof categories.items === "undefined" && data) setCategories(data);
+	}, [data]);
 
 	console.log(data);
 
@@ -53,8 +54,21 @@ export default function Home({ data }: any) {
 
 			<main className={styles.main}>
 				<h1>Categories:</h1>
-				{categories &&
-					categories.items.map((category: any) => <li>{category?.name}</li>)}
+				<div className={styles.results}>
+					{typeof categories.items !== "undefined"
+						? categories.items.map((category: any) => (
+								<div className={styles.categories}>
+									<li key={category?.id}>{category?.name}</li>
+									<Image
+										alt="category icon"
+										src={category.icons?.[0].url}
+										height={275}
+										width={275}
+									/>
+								</div>
+						  ))
+						: "Loading..."}
+				</div>
 			</main>
 		</div>
 	);
