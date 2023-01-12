dc083c35ded2be3c3be3e54e2ec546338d09077c
Zac Cooper <zacooper5g@outlook.com>
2023-01-12 11:44:17 +1100

    app now pulls in basic information from spotify API

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import React, { useState } from "react";
+import MusicContext from "../context/MusicContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	const [categories, setCategories] = useState<any>([]);
+
+	return (
+		<MusicContext.Provider value={{ categories, setCategories }}>
+			<Component {...pageProps} />
+		</MusicContext.Provider>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,49 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import React, { useContext, useEffect } from "react";
+import MusicContext from "../context/MusicContext";
+
+const getToken = async () => {
+	let response = await fetch("https://accounts.spotify.com/api/token", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+			Authorization: `Basic ${btoa(
+				`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
+			)}`,
+		},
+		body: "grant_type=client_credentials",
+	});
+	let data = await response.json();
+	return data.access_token;
+};
 
 export const getStaticProps = async () => {
-	let response = await fetch("https://api.spotify.com/v1");
+	let token = await getToken();
+	let response = await fetch(`https://api.spotify.com/v1/browse/categories`, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
 	let data = await response.json();
 
 	return {
 		props: {
-			data: data,
+			data: data.categories,
 		},
 	};
 };
 
 export default function Home({ data }: any) {
-  console.log(data);
+	const musicContext = useContext(MusicContext);
+	const { categories, setCategories } = musicContext;
+
+	useEffect(() => {
+		setCategories(data);
+	}, []);
+
+	console.log(data);
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -21,7 +51,11 @@ export default function Home({ data }: any) {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className={styles.main}>YOUTUBE</main>
+			<main className={styles.main}>
+				<h1>Categories:</h1>
+				{categories &&
+					categories.items.map((category: any) => <li>{category?.name}</li>)}
+			</main>
 		</div>
 	);
 }
