969e5211d66b7bfcb6fe5e391d23679e45c69fe8
Zac Cooper <zacooper5g@outlook.com>
2023-01-13 13:22:46 +1100

    added featured playlist global state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,19 @@ import React, { useState } from "react";
 import MusicContext from "../context/MusicContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-	const [categories, setCategories] = useState<any>([]);
+  const [categories, setCategories] = useState<any>([]);
+  const [featuredPlaylists, setFeaturedPlaylists] = useState([]);
 
-	return (
-		<MusicContext.Provider value={{ categories, setCategories }}>
-			<Component {...pageProps} />
-		</MusicContext.Provider>
-	);
+  return (
+    <MusicContext.Provider
+      value={{
+        categories,
+        setCategories,
+        featuredPlaylists,
+        setFeaturedPlaylists,
+      }}
+    >
+      <Component {...pageProps} />
+    </MusicContext.Provider>
+  );
 }
